1012cb4860c4263eed20f61a28cb22aa32236ceb
Marcos Vinicius Conterno de Souza <mvcs1996@hotmail.com>
2025-02-13 18:24:06 -0300

    Commit Docker

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // Gera a build standalone para Docker
 };
 
 export default nextConfig;
